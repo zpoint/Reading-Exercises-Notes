bececd56482442c94385ff86d9a0cdef7159936f
zpoint <zp@zp0int.com>
2016-11-15 22:47:16 +0800

    C++ Primer Chapter 6 0.1

---
+++ C++ Primer/6_Functions/6_2.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+// a
+int f()
+{
+	int s;
+	return s;
+}
+
+// b
+void f2(int i)
+{
+	
+}
+
+// c
+int calc(int v1, int v2)
+{
+	return v1;
+}
+
+// d
+double square(double x)
+{
+	return x * x;
+}
+
+int main()
+{
+	return 0;
+}
