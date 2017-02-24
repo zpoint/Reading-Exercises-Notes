0a442a6b7cdd46df75812e2de585a9ed816521ca
zpoint <zp@zp0int.com>
2017-02-24 23:30:35 +0800

    C++ Primer 13.1.5

---
+++ C++ Primer/13_Copy_Assign_and_Destory/13_15.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+int global_int = 0;
+
+class numbered {
+		public:
+				numbered(): mysn(++global_int) { }
+				numbered(numbered&): mysn(++global_int) { }
+				int mysn;
+};
+
+void f (numbered s) { std::cout << s.mysn << std::endl; }
+
+int main()
+{
+		numbered a, b = a, c = b;
+		f(a); f(b); f(c);
+		return 0;
+}
