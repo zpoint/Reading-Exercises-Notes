df7f677b0a43e9d4c52b2fda543db76581fd6ecb
zpoint <zp@zp0int.com>
2018-10-16 18:53:56 +0800

    smart pointer done

---
+++ The_Boost_C++_Libraries/SmartPointers/shared_ptr_ALLOCATOR.cpp
@@ -0,0 +1,15 @@
+#define BOOST_SP_USE_QUICK_ALLOCATOR
+#include <boost/shared_ptr.hpp>
+#include <iostream>
+#include <ctime>
+
+int main()
+{
+		boost::shared_ptr<int> p;
+		std::time_t then = std::time(nullptr);
+		for (int i = 0; i < 10000000; ++i)
+				p.reset(new int{i});
+		std::time_t now = std::time(nullptr);
+		std::cout << now - then << '\n';
+		return 0;
+}
