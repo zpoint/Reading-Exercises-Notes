b1290da9dbbb7095c0680a74e06691ac367a572b
zpoint <zp@zp0int.com>
2018-10-17 19:01:26 +0800

    ScopeExit

---
+++ The_Boost_C++_Libraries/PointerContainer/ptr_vector.cpp
@@ -0,0 +1,12 @@
+#include <boost/ptr_container/ptr_vector.hpp>
+#include <iostream>
+
+int main()
+{
+		boost::ptr_vector<int> v;
+		v.push_back(new int{1});
+		v.push_back(new int{2});
+		std::cout << v.back() << '\n';
+		std::cout << *v.begin() << '\n';
+		return 0;
+}
