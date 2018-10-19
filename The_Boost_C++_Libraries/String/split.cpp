d2d3983d637e09b6c486c3ff0d467f87b6b2f952
zpoint <zp@zp0int.com>
2018-10-19 19:20:44 +0800

    string

---
+++ The_Boost_C++_Libraries/String/split.cpp
@@ -0,0 +1,15 @@
+#include <boost/algorithm/string.hpp>
+#include <string>
+#include <vector>
+#include <iostream>
+
+using namespace boost::algorithm;
+
+int main()
+{
+		std::string s = "Boost C++ Libraries";
+		std::vector<std::string> v;
+		split(v, s, is_space());
+		std::cout << v.size() << '\n';
+		return 0;
+}
---
+++ The_Boost_C++_Libraries/String/uppercase_locale.cpp
@@ -0,0 +1,17 @@
+#include <boost/algorithm/string.hpp>
+#include <string>
+#include <locale>
+#include <iostream>
+
+using namespace boost::algorithm;
+
+int main()
+{
+		std::string s = "Boost C++ k\xfct\xfcphaneleri";
+		std::string upper_case1 = to_upper_copy(s);
+		std::string upper_case2 = to_upper_copy(s, std::locale{"tr_TR"});
+		std::locale::global(std::locale{"tr_TR"});
+		std::cout << upper_case1 << '\n';
+		std::cout << upper_case2 << '\n';
+		return 0;
+}
