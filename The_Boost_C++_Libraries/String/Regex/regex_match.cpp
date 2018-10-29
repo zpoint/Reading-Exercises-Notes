e28cde56c550eadeb650aaa5a41548d88632c43a
zpoint <zp@zp0int.com>
2018-10-29 18:58:54 +0800

    spirit

---
+++ The_Boost_C++_Libraries/String/Regex/regex_match.cpp
@@ -0,0 +1,11 @@
+#include <boost/regex.hpp>
+#include <string>
+#include <iostream>
+
+int main()
+{
+		std::string s = "Boost Libraries";
+		boost::regex expr{"\\w+\\s\\w+"};
+		std::cout << std::boolalpha << boost::regex_match(s, expr) << '\n';
+		return 0;
+}
---
+++ The_Boost_C++_Libraries/String/Regex/regex_replace.cpp
@@ -0,0 +1,12 @@
+#include <boost/regex.hpp>
+#include <string>
+#include <iostream>
+
+int main()
+{
+		std::string s = " Boost Libraries ";
+		boost::regex expr{"\\s"};
+		std::string fmt{"_"};
+		std::cout << boost::regex_replace(s, expr, fmt) << '\n';
+		return 0;
+}
