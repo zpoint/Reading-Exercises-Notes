5afe85d18cd74f376d4118b9c0814f0c27078cd4
zpoint <zp@zp0int.com>
2018-11-20 18:39:26 +0800

    GenericProgramming

---
+++ The_Boost_C++_Libraries/GenericProgramming/LanguageExtension/dynamic_cast.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+struct base1 { virtual ~base1() = default; };
+struct base2 { virtual ~base2() = default; };
+struct derived : public base1, public base2 {};
+
+void downcast(base1 *b1)
+{
+		derived *d = dynamic_cast<derived*>(b1);
+		std::cout << d << std::endl;
+}
+
+void crosscast(base1 *b1)
+{
+		base2 *b2 = dynamic_cast<base2*>(b1);
+}
+
+int main()
+{
+		derived *d = new derived;
+		downcast(d);
+
+		base1 *b1 = new derived;
+		crosscast(b1);
+		return 0;
+}
+
---
+++ The_Boost_C++_Libraries/GenericProgramming/LanguageExtension/polymorphic_cast.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <boost/cast.hpp>
+
+struct base1 { virtual ~base1() = default; };
+struct base2 { virtual ~base2() = default; };
+struct derived : public base1, public base2 {};
+
+void downcast(base1 *b1)
+{
+		derived *d = boost::polymorphic_downcast<derived*>(b1);
+		std::cout << d << std::endl;
+}
+
+void crosscast(base1 *b1)
+{
+		base2 *b2 = boost::polymorphic_cast<base2*>(b1);
+}
+
+int main()
+{
+		derived *d = new derived;
+		downcast(d);
+
+		base1 *b1 = new derived;
+		crosscast(b1);
+		return 0;
+}
+
