3e2778a2c38dca3c3ba4cec36118d490ca8c40d7
zpoint <zp@zp0int.com>
2018-11-23 18:35:05 +0800

    ApplicationLibrarise

---
+++ The_Boost_C++_Libraries/ApplicationLibrarise/UUID/random_generator.cpp
@@ -0,0 +1,14 @@
+#include <boost/uuid/uuid.hpp>
+#include <boost/uuid/uuid_generators.hpp>
+#include <boost/uuid/uuid_io.hpp>
+#include <iostream>
+
+using namespace boost::uuids;
+
+int main()
+{
+		random_generator gen;
+		uuid id = gen();
+		std::cout << id << '\n';
+		return 0;
+}
