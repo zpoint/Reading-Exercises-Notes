e96844e07275ed4e84ebc9377454d20b146910e6
zpoint <zp@zp0int.com>
2019-09-25 18:54:02 +0800

    C++_Concurrency_in_Action chapter 5 done

---
+++ C++_Concurrency_in_Action/The_C++_memory_model_and_operations_on_atomic_types/acquire_relax_good.cpp
@@ -0,0 +1,37 @@
+#include <atomic>
+#include <thread>
+#include <assert.h>
+
+std::atomic<bool> x, y;
+std::atomic<int> z;
+
+void write_x_then_y()
+{
+		x.store(true, std::memory_order_relaxed);
+		y.store(true, std::memory_order_release);
+}
+
+void read_y_then_x()
+{
+		while(!y.load(std::memory_order_acquire));
+		if (x.load(std::memory_order_relaxed))
+				++z;
+}
+
+int main()
+{
+		x = false;
+		y = false;
+		z = 0;
+		std::thread a(write_x_then_y);
+		std::thread b(read_y_then_x);
+		a.join();
+		b.join();
+		assert(z.load() != 0);
+		/*
+		 * store to x happens-before the store to y, they're in the same thread.
+		 * store to y synchronizes-wit the load from y, the store to x alse happens-before
+		 * the load from y and by extension happens-before the load from x
+		 */
+		return 0;
+}
